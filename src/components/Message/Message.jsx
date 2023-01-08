fa5097f67b58819266098e92d67f9dcb25020af0
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-08 23:36:22 +0300

    feat: implement mock data in messages component

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 import unknownAvatar from '../../assets/images/unknown_avatar.png'
 import styles from './Message.module.css'
 
-const Message = ({ linkToAvatar = unknownAvatar, message, date }) => {
+const Message = ({ avatar = unknownAvatar, message, date }) => {
 
   const timeElapsed = Date.now()
   const today = new Date(timeElapsed).toDateString()
 
   return (
-    <li className={styles.wrapper}>
-      <img src={linkToAvatar} alt='user avatar' className={styles.avatar} />
+    <>
+      <img src={avatar} alt='user avatar' className={styles.avatar} />
       <span className={styles.message}>{message}</span>
       <div className={styles.date}>{date ?? today}</div>
-    </li>
+    </>
   )
 }
 
--- src/pages/MessagingPage/MessagingPage.jsx
+++ src/pages/MessagingPage/MessagingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
+import Correspondence from '../../components/Correspondence/Correspondence'
 import Dialogs from '../../components/Dialogs/Dialogs'
-import Message from '../../components/Message/Message'
 
 import styles from './MessagingPage.module.css'
 
@@ -9,20 +9,7 @@ const MessagingPage = () => {
   return (
     <div className={styles.wrapper}>
       <Dialogs />
-      <ul>
-        <li>
-          <Message message='Hey! How are you?' />
-        </li>
-        <li>
-          <Message message="Thank you. I'm fine." />
-        </li>
-        <li>
-          <Message message="What's the latest news?" />
-        </li>
-        <li>
-          <Message message='Grass in the yard, firewood on the grass' />
-        </li>
-      </ul>
+      <Correspondence />
     </div >
   )
 }
