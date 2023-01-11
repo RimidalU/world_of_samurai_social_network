6934675a385c2b4692d86016d515c3029e42d135
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-11 16:45:19 +0300

    feat: refactor AddPost component

--- src/layouts/AddPost/AddPost.jsx
+++ src/layouts/AddPost/AddPost.jsx
@@ -1,29 +1,39 @@
 import React from 'react'
+import Button from '../../components/Button/Button'
+import Textarea from '../../components/Textarea/Textarea'
 
 import styles from './AddPost.module.css'
 
 const textarea = React.createRef()
 
 const AddPost = () => {
+
+  function addPost() {
+    alert(textarea.current.value)
+    removePost()
+  }
+
+  function removePost() {
+    textarea.current.value = ''
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <form >
-        <p>
-          <label htmlFor="addPost">
-            Add new post
-          </label>
-        </p>
-        <textarea ref={textarea}
-          rows='2'
-          // cols='150'
-          name='addPost'
-          label='Input post'
-        ></textarea>
-        <p>
-          <input type="submit" value="Add Post" onClick={() => alert(textarea.current.value)}/>
-        </p>
-      </form>
-    </div>
+    <form className={styles.wrapper}>
+      <Textarea
+        refer={textarea}
+        placeholder='Add thought'
+        id='addPost'
+      />
+      <span className={styles.buttonWrapper}>
+        <Button onClick={removePost} >
+          Erase stupidity
+        </Button>
+        <Button onClick={addPost}>
+          Declare wisdom
+        </Button>
+      </span>
+    </form>
+    // </div>
   )
 }
 
