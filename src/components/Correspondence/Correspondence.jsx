d58439034573d3b3e1bc271dc333258cdec0f81f
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-14 23:26:45 +0300

    feat: add CorrespondenceSet component

--- src/components/Correspondence/Correspondence.jsx
+++ src/components/Correspondence/Correspondence.jsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import AddNoteContainer from '../../layouts/AddNote/AddNoteContainer'
-
-import Message from '../Message/Message'
+import CorrespondenceSet from '../CorrespondenceSet/CorrespondenceSet'
 
 import styles from './Correspondence.module.css'
 
-const Correspondence = ({store}) => {
+const Correspondence = ({ store }) => {
   const correspondence = store.getState().messagingPage.correspondence
   return (
     <div className={styles.wrapper}>
       <AddNoteContainer componentType='message' store={store} />
-      <ul className={styles.dialogs}>
-        {
-          correspondence.map(message => (
-            <li key={message.id} className={styles.messageWrapper}>
-              <Message {...message} />
-            </li>
-          ))
-        }
-      </ul >
+      <CorrespondenceSet correspondence={correspondence} />
     </div>
 
   )
---
+++ src/components/CorrespondenceSet/CorrespondenceSet.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Message from '../Message/Message'
+
+import styles from './CorrespondenceSet.module.css'
+
+const CorrespondenceSet = ({ correspondence }) => {
+
+  return (
+    <ul className={styles.wrapper}>
+    {
+      correspondence.map(message => (
+        <li key={message.id} className={styles.message}>
+          <Message {...message} />
+        </li>
+      ))
+    }
+  </ul >
+  )
+}
+
+export default CorrespondenceSet
