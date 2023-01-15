6746151d645fb576124754f89f5cd73400785f2f
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-15 23:39:22 +0300

    feat: add react-redux lib

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
-import { Provider } from './StoreContext'
+import { Provider } from 'react-redux'
 
 import store from './redux/store'
 
