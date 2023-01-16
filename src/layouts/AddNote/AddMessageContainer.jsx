18cb473367dd35a0204f863a8908f86c2ef16eb8
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-16 11:48:52 +0300

    feat: refactor AddMessageContainer component to connect react-redux

--- src/layouts/AddNote/AddMessageContainer.jsx
+++ src/layouts/AddNote/AddMessageContainer.jsx
@@ -1,36 +1,28 @@
-import React, { useContext } from 'react'
+import { connect } from 'react-redux'
 
 import { addMessage, updateMessage } from '../../handmadeRedux/actionCreator'
-import { StoreContext } from '../../StoreContext'
 import AddNote from './AddNote'
 
-const AddMessageContainer = () => {
-
-  const store = useContext(StoreContext)
-
-  let newNoteText = store.getState().messagingPage.newMessageText
-
-  function addNote() {
-    store.dispatch(addMessage())
-    removeNote()
+const mapStateToProps = (state) => {
+  return {
+    newNoteText: state.messagingPage.newMessageText
   }
-
-  function removeNote() {
-    store.dispatch(updateMessage(''))
-  }
-
-  function changeNote(noteText) {
-    store.dispatch(updateMessage(noteText))
-  }
-
-  return (
-    <AddNote
-      changeNote={changeNote}
-      addNote={addNote}
-      newNoteText={newNoteText}
-      removeNote={removeNote}
-    />
-  )
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addNote: () => {
+      dispatch(addMessage())
+      dispatch(updateMessage(''))
+    },
+    changeNote: (noteText) => {
+      dispatch(updateMessage(noteText))
+    },
+    removeNote: () => {
+      dispatch(updateMessage(''))
+    }
+  }
+}
+const AddMessageContainer = connect(mapStateToProps, mapDispatchToProps)(AddNote)
 
 export default AddMessageContainer
