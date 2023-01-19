e046a6cf36fe9027c1ba5efcda93a055627257d8
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-20 00:24:22 +0300

    rafactor: refactor Button component style

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 import styles from './Button.module.css'
 
-const Button = ({ type = 'button', onClick, children, ...rest }) => {
+const Button = ({ type = 'button', isSelected = false, onClick, children, ...rest }) => {
   return (
-    <button type={type} onClick={onClick} {...rest} className={styles.button} >
+    <button type={type} onClick={onClick} {...rest} className={`${styles.button} ${isSelected && styles.selected}`} >
       {children}
     </button>
-  );
-};
+  )
+}
 
 export default Button
