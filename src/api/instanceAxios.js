5a154e1596c906541be10d224f1cd2dc1068a66f
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-01-23 17:50:11 +0300

    feat: implement .env

--- src/api/instanceAxios.js
+++ src/api/instanceAxios.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 
 const instanceAxios = axios.create({
   withCredentials: true,
-  baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
-    'API-KEY': '3a85fa12-3a31-4f05-9860-0522a4d39480'
+    'API-KEY': process.env.REACT_APP_API_KEY
   }
 })
 
